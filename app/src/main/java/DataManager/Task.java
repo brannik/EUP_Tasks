52bedef7e5e7455d33bd9091d11288468a39e7c4
Georgi Golemshinski <georgi.vtu.mi@gmail.com>
2023-06-13 08:54:08 -0700

    update

--- app/src/main/java/DataManager/Task.java
+++ app/src/main/java/DataManager/Task.java
@@ -1,4 +1,10 @@
 package DataManager;
 
+import java.util.List;
+
 public class Task {
+    public List<String> WhoCanSee; // list of user id's
+    public String TaskName;
+    public String TaskDescription;
+    // to do
 }
